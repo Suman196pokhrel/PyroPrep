46b7372428454983c276751c17a8f69af6fc0b58
Suman pokhrel <spokhrel196@gmail.com>
2024-03-20 21:58:34 +0545

    added custom checkbox

--- app/dashboard/[domainId]/[subDomainId]/page.tsx
+++ app/dashboard/[domainId]/[subDomainId]/page.tsx
@@ -63,6 +63,8 @@ const SubDomainPage = ({ params }: { params: { subDomainId: string } }) => {
 
 
             <div className='flex flex-row items-start'>
+
+                {/* CARDS  */}
                 <div className=' flex flex-wrap gap-10 w-3/4'>
                     {/* DOMAIN CARDS  */}
                     {mockTestSets.map((items, index) => (
@@ -79,13 +81,14 @@ const SubDomainPage = ({ params }: { params: { subDomainId: string } }) => {
                     ))}
                 </div>
 
+                {/* OPTIONS  */}
                 <div className='flex flex-col text-[#005249] items-start pl-5 w-1/4 h-full border-l-2 border-gray-200 gap-10'>
                     {/* FILTERS  */}
                     <div className='flex flex-col'>
                         <h2 className='text-[#3d4e61] font-bold text-3xl'>Filters</h2>
                         <div className='flex flex-col gap-3 mt-5'>
                             <div className='flex items-center gap-2'>
-                                <Checkbox id="terms" className=' w-5 h-5 text-[#005249]' />
+                                <Checkbox id="terms" className=' w-6 h-6 text-white border-gray-300 data-[state=checked]:bg-[#229A16] data-[state=checked]:text-white ' />
                                 <Label
                                     htmlFor="terms"
                                     className="text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -94,7 +97,7 @@ const SubDomainPage = ({ params }: { params: { subDomainId: string } }) => {
                                 </Label>
                             </div>
                             <div className='flex items-center gap-2'>
-                                <Checkbox id="terms" className=' w-5 h-5 text-[#005249]' />
+                                <Checkbox id="terms" className=' w-6 h-6 text-white border-gray-300 data-[state=checked]:bg-[#229A16] data-[state=checked]:text-white ' />
                                 <Label
                                     htmlFor="terms"
                                     className="text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -110,7 +113,7 @@ const SubDomainPage = ({ params }: { params: { subDomainId: string } }) => {
                         <h2 className='text-[#3d4e61] font-bold text-3xl'>Sort</h2>
                         <div className='flex flex-col gap-3 mt-5'>
                             <div className='flex items-center gap-2'>
-                                <Checkbox id="terms" className=' w-5 h-5 text-[#005249]' />
+                                <Checkbox id="terms" className=' w-6 h-6 text-white border-gray-300 data-[state=checked]:bg-[#229A16] data-[state=checked]:text-white ' />
                                 <Label
                                     htmlFor="terms"
                                     className="text-md  font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -119,7 +122,7 @@ const SubDomainPage = ({ params }: { params: { subDomainId: string } }) => {
                                 </Label>
                             </div>
                             <div className='flex items-center gap-2'>
-                                <Checkbox id="terms" className=' w-5 h-5 text-[#005249]' />
+                                <Checkbox id="terms" className=' w-6 h-6 text-white border-gray-300 data-[state=checked]:bg-[#229A16] data-[state=checked]:text-white ' />
                                 <Label
                                     htmlFor="terms"
                                     className="text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -132,8 +135,6 @@ const SubDomainPage = ({ params }: { params: { subDomainId: string } }) => {
                 </div>
 
             </div>
-
-
 
         </main>
     )
