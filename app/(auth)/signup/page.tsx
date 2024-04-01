d1d88218163b2f8a65cf1aadbe0c06a2cf087684
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-01 11:28:00 +0545

    restructured signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,34 +1,34 @@
 "use client"
-import { Button } from '@/components/atoms/button'
-import { Checkbox } from '@/components/atoms/checkbox'
+import { CardWrapper } from '@/components/auth/CardWrapper'
 import AuthHeader from '@/components/molecules/AuthHeader'
 import LoginMethods from '@/components/molecules/LoginMethods'
 import SignUpForm from '@/components/organisms/SignUpForm'
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const SignUp = () => {
-    const currentPath = usePathname()
     return (
-        <main className='flex flex-col items-center w-full space-y-5 '>
-            {/* HEADER  */}
-            <AuthHeader
-                title='Get started absoluetly free.'
-                subTitle='Join us and explore more!.'
-            />
 
-            {/* LOGIN METHODS  */}
-            <LoginMethods />
+
+        < CardWrapper
+            headerLabel='Get started absoluetly free.'
+            headerSubLabel='Join us and explore more!.'
+            backButtonLabel="Already have an account?"
+            backButtonHref='/signin'
+            showSocial
+        >
+
+
 
             {/* FORM  */}
             <SignUpForm />
 
+        </CardWrapper>
 
 
 
 
-        </main>
+
     )
 }
 
