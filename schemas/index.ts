54adfa24d1b287540e1b7fb5e30d1f075098cec6
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-01 11:31:57 +0545

    updated register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,9 @@ export const LoginSchema = z.object({
   email: z.string().email("Invalid email format"),
   password: z.string().min(1, { message: "Password is required" }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email("Invalid email format").min(1, "Email is required"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+  name: z.string().min(1, "Name must be at least 1 characters long"),
+});
