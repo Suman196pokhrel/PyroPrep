27cedcedb5a05ddedadac184bb255d573c81290e
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-01 12:27:51 +0545

    added server action logic to create new user via email and passowrd

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   // SERVER SIDE VALIDATION
@@ -11,6 +13,31 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  console.log(values);
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+
+  // HASH PASSWD
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // FIRST CHECK IF THE EMAIL IS ALREADY TAKEN
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  // CREATE NEW USER
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: SEND EMAILVERIFICATION TOKEN
+  return { success: "New user registeresd!" };
 };
