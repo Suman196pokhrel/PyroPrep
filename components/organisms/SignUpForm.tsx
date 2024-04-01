cfc8d2765803b208e9f1a5c4360abfa70f3445db
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-01 11:41:50 +0545

    added server actions for signup form

--- components/organisms/SignUpForm.tsx
+++ components/organisms/SignUpForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useTransition, useState } from 'react'
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -8,12 +8,20 @@ import { Input } from '../atoms/input'
 import { Button } from '../atoms/button'
 import { useRouter } from 'next/navigation'
 import { RegisterSchema } from '@/schemas'
+import { register } from '@/actions/register'
+import { FormError } from '../FormError'
+import { FormSuccess } from '../FormSuccess'
+
 
 
 
 const SignUpForm = () => {
 
     const router = useRouter()
+    const [isPending, startTransition] = useTransition()
+    const [error, setError] = useState<string | undefined>("")
+    const [success, setSuccess] = useState<string | undefined>("")
+
 
     const form = useForm<z.infer<typeof RegisterSchema>>({
         resolver: zodResolver(RegisterSchema),
@@ -26,6 +34,16 @@ const SignUpForm = () => {
 
     function onSubmit(values: z.infer<typeof RegisterSchema>) {
         console.log(values)
+        setError("")
+        setSuccess("")
+
+        startTransition(() => {
+            register(values)
+                .then((data) => {
+                    setError(data.error)
+                    setSuccess(data.success)
+                })
+        })
         // router.push("/signin")
     }
 
@@ -34,47 +52,51 @@ const SignUpForm = () => {
         <Form {...form}>
 
             <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4  xl:space-y-7 w-full text-lg'>
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input className='h-14 text-lg xl:h-16' placeholder='John' {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input className='h-14 text-lg xl:h-16' placeholder='john.doe!example.com' type='email' {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+                <div>
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input className='h-14 text-lg xl:h-16' placeholder='John' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input className='h-14 text-lg xl:h-16' placeholder='john.doe!example.com' type='email' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input className='h-14 text-lg xl:h-16' placeholder='******' type='password' {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type='submit' className='w-full  h-14 text-lg xl:h-16 xl:text-xl' variant={"pyroPrimary"}>Create account</Button>
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input className='h-14 text-lg xl:h-16' placeholder='******' type='password' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <FormError message={error} />
+                <FormSuccess message={success} />
+                <Button type='submit' className='w-full  h-14 text-lg xl:h-16 xl:text-xl' variant={"pyroPrimary"}>Create an account</Button>
 
             </form>
 
