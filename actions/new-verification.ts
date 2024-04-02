32fbaabf57b4e35abcb04e12a077e94b2d4a7ffe
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-02 15:41:05 +0545

    email verification logic added

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -5,7 +5,9 @@ import { getVerificationTokenByToken } from "@/data/verification-token";
 
 export const newVerification = async (token: string) => {
   const existingToken = await getVerificationTokenByToken(token);
-  if (!existingToken) return { error: "Token does not exist!" };
+  if (!existingToken) {
+    return { error: "Token does not exist!" };
+  }
 
   const hasExpired = new Date(existingToken.expires) < new Date();
 
--- app/(auth)/new-verification/page.tsx
+++ app/(auth)/new-verification/page.tsx
@@ -1,16 +1,39 @@
 "use client"
 import { CardWrapper } from '@/components/auth/CardWrapper'
 import { PropagateLoader } from "react-spinners"
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { newVerification } from '@/actions/new-verification'
+import { FormSuccess } from '@/components/FormSuccess'
+import { FormError } from '@/components/FormError'
 
 export const NewVerificationPage = () => {
     const searchParams = useSearchParams()
     const token = searchParams.get("token")
+    const [error, setError] = useState<string | undefined>()
+    const [success, setSuccess] = useState<string | undefined>()
+
 
     const onSubmit = useCallback(() => {
-        console.log(token)
-    }, [token])
+        if (success || error) return;
+
+        // CHECK IF TOKEN HAS EXPIRE 
+        if (!token) {
+            setError("Missing token")
+            return
+        }
+
+        newVerification(token)
+            .then((data) => {
+                setSuccess(data.success);
+                setError(data.error);
+            })
+            .catch(() => {
+                setError("Something went wrong!");
+            })
+    }, [token, success, error]);
+
+
 
 
     useEffect(() => {
@@ -27,7 +50,13 @@ export const NewVerificationPage = () => {
         >
 
             <div className='flex items-center w-full justify-center py-5'>
-                <PropagateLoader color="#2eb82e" className='w-10 h-10' />
+                {!success && !error && (
+                    <PropagateLoader color="#2eb82e" className='w-10 h-10' />
+                )}
+                <FormSuccess message={success} />
+                {!success && (
+                    <FormError message={error} />
+                )}
             </div>
         </CardWrapper >
     )
