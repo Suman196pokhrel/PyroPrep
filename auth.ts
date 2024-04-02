967571fede3999de711ed018270a2a6cba5d591f
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-02 08:07:33 +0545

    added auto emailVerified populate feature for users who login with oAuth providers

--- auth.ts
+++ auth.ts
@@ -10,6 +10,15 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  events: {
+    // TO AUTO POPULATE EMAIL VERIFIED FOR USERS WHO REGISTER OR LOGIN WITH OAUTH PROVIDERS
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     async signIn({ user }) {
       if (!user.id) return false;
