6a986cb59424a2abea3c1db88645cf1312bc7b3c
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-02 15:00:03 +0545

    server actions added to handle email verification

--- app/(auth)/new-verification/page.tsx
+++ app/(auth)/new-verification/page.tsx
@@ -1,9 +1,23 @@
 "use client"
 import { CardWrapper } from '@/components/auth/CardWrapper'
 import { PropagateLoader } from "react-spinners"
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 export const NewVerificationPage = () => {
+    const searchParams = useSearchParams()
+    const token = searchParams.get("token")
+
+    const onSubmit = useCallback(() => {
+        console.log(token)
+    }, [token])
+
+
+    useEffect(() => {
+        onSubmit()
+    }, [onSubmit])
+
+
     return (
         <CardWrapper
             headerLabel='Confirming your verification'
