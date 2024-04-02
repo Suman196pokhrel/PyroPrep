c91d3cde9390bcf00e42593575a43049ec7f9a47
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-02 07:22:54 +0545

    extended token with user role field

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
+import { getUserById } from "./data/users";
 
 export const {
   handlers: { GET, POST },
@@ -21,6 +22,12 @@ export const {
 
     async jwt({ token }) {
       // console.log("TOKEN ", token);
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
       return token;
     },
   },
