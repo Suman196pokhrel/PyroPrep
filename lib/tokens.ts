96cd803755658c254e39814ab559d8539981546f
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-03 08:50:18 +0545

    updated schema name

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,7 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "@/lib/db";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -10,7 +11,7 @@ export const generateVerificationToken = async (email: string) => {
   const existingToken = await getVerificationTokenByEmail(email);
 
   if (existingToken) {
-    const deleteToken = await db.verificationToken.delete({
+    await db.verificationToken.delete({
       where: { id: existingToken.id },
     });
   }
@@ -25,4 +26,29 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000); // expire token in 1 hr - number of miliseconds
+
+  // CHECK IF THIS TOKEN ALREADY EXISTS
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  //   PUSH NEW TOKEN TO DB
+  const passwordResetToken = await db.passwordToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
 };
