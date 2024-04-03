82603332ec2f81660f622d7cf023ee0998402c41
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-03 08:44:33 +0545

    added new util functions to get passwordresettokens

---
+++ data/password-reset-token.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+
+export const getPasswordResetTokenByToken = async (token: string) => {
+  try {
+    const passwordResetToken = await db.passwordToken.findUnique({
+      where: { token },
+    });
+
+    return passwordResetToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getPasswordResetTokenByEmail = async (email: string) => {
+  try {
+    const passwordResetToken = await db.passwordToken.findFirst({
+      where: { email },
+    });
+
+    return passwordResetToken;
+  } catch (error) {
+    return null;
+  }
+};
