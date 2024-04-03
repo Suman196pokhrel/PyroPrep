ae7cea049dd531317631782f669247ae735f29d9
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-03 08:53:03 +0545

    added new function inside mails to sendPasswordResetEmail

--- lib/mail.ts
+++ lib/mail.ts
@@ -26,3 +26,28 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     .then((value) => console.log(value))
     .catch((error) => console.log(error));
 };
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const baseConfirmLink = isDevelopment
+    ? process.env.VERIFY_EMAIL_TOKEN_CONFIRM_LINK_LOCAL
+    : process.env.VERIFY_EMAIL_TOKEN_CONFIRM_LINK_PROD;
+
+  const fromEmail = process.env.FROM_EMAIL || "onboarding@resend.dev";
+
+  const confirmLink = `${baseConfirmLink}?token=${token}`;
+
+  await resend.emails
+    .send({
+      from: fromEmail,
+      to: email,
+      subject: "Reset you account password",
+      html: `
+    <p>
+    Click on on the <a href="${confirmLink}">Link</a> to create new password for your pyroprep account
+    </P>
+    `,
+    })
+    .then((value) => console.log(value))
+    .catch((error) => console.log(error));
+};
