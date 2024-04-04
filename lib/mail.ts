74a769d3df7f0474e82a69f53b6e5d9713bca36a
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-04 08:43:41 +0545

    solved prod bugs

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,14 +1,26 @@
 import { Resend } from "resend";
+import { MailtrapClient } from "mailtrap";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const fromEmail = isDevelopment
+  ? process.env.FROM_EMAIL_LOCAL || "onboarding@resend.dev"
+  : process.env.FROM_EMAIL_PROD || "onboarding@resend.dev";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const MAILTRAP_TOKEN = process.env.MAILTRAP_API_TOKEN || " ";
+const MAILTRAP_SENDER_EMAIL = process.env.MAILTRAP_SENDER_EMAIL || "";
+const MAILTRAP_ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+const MAILTRAP_SENDER = {
+  name: "Pyroprep",
+  email: MAILTRAP_SENDER_EMAIL,
+};
+const mailtrapClient = new MailtrapClient({ token: MAILTRAP_TOKEN });
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const isDevelopment = process.env.NODE_ENV === "development";
   const baseConfirmLink = isDevelopment
     ? process.env.VERIFY_EMAIL_TOKEN_CONFIRM_LINK_LOCAL
     : process.env.VERIFY_EMAIL_TOKEN_CONFIRM_LINK_PROD;
-
-  const fromEmail = process.env.FROM_EMAIL || "onboarding@resend.dev";
 
   const confirmLink = `${baseConfirmLink}?token=${token}`;
 
@@ -25,17 +37,26 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     })
     .then((value) => console.log(value))
     .catch((error) => console.log(error));
+
+  // await mailtrapClient
+  //   .send({
+  //     from: MAILTRAP_SENDER,
+  //     to: [{ email }],
+  //     subject: "Confirm your email",
+  //     html: `
+  //   <p>
+  //   Click on on the <a href="${confirmLink}">Link</a> to activate your pyroprep account
+  //   </P>
+  //   `,
+  //   })
+  //   .then((value) => console.log(value))
+  //   .catch((error) => console.log(error));
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const isDevelopment = process.env.NODE_ENV === "development";
   const baseResetLink = isDevelopment
     ? process.env.PASSWORD_RESET_TOKEN_CONFIRM_LINK_LOCAL
     : process.env.PASSWORD_RESET_TOKEN_CONFIRM_LINK_PROD;
-
-  const fromEmail = isDevelopment
-    ? process.env.FROM_EMAIL_LOCAL || "onboarding@resend.dev"
-    : process.env.FROM_EMAIL_PROD || "onboarding@resend.dev";
 
   const resetLink = `${baseResetLink}?token=${token}`;
 
@@ -52,4 +73,18 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     })
     .then((value) => console.log(value))
     .catch((error) => console.log(error));
+
+  // await mailtrapClient
+  //   .send({
+  //     from: MAILTRAP_SENDER,
+  //     to: [{ email }],
+  //     subject: "Reset you account password",
+  //     html: `
+  //   <p>
+  //   Click on on the <a href="${resetLink}">Link</a> to create new password for your pyroprep account
+  //   </P>
+  //   `,
+  //   })
+  //   .then((value) => console.log(value))
+  //   .catch((error) => console.log(error));
 };
