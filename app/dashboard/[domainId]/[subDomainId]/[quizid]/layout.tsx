f36128a7aed71349b4fc8de298fc38edb2d02e3e
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-07 09:19:41 +0545

    added additional routes in dashboard

--- app/dashboard/[domainId]/[subDomainId]/[quizid]/layout.tsx
+++ app/dashboard/[domainId]/[subDomainId]/[quizid]/layout.tsx
@@ -4,15 +4,21 @@ import React from 'react'
 
 type IndiQuizLayoutProps = {
     children: React.ReactNode
+    params: {
+        quizid: string
+    }
+
 }
 
 
 const IndiQuizLayout = ({
-    children
+    children,
+    params,
 }: IndiQuizLayoutProps) => {
     return (
-        <div className='border-2 border-red-600'>
+        <div className='border-2 border-red-600 w-full min-h-screen'>
             <h2>Inside indi quiz layout</h2>
+            <p>{params.quizid}</p>
             <div>
                 {children}
             </div>
