00bdb5285dd32e2130b2f22924a5fc9a4bdde9b2
Suman Pokhrel <spokhrel196@gmail.com>
2024-04-12 07:40:02 +0545

    added loader on signin landing page

--- components/auth/LoginButton.tsx
+++ components/auth/LoginButton.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
+import { MoonLoader } from 'react-spinners'
 
 interface LoginButtonProps {
     children: React.ReactNode,
@@ -15,10 +16,18 @@ const LoginButton = ({
 }: LoginButtonProps) => {
 
     const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
 
-    const onClick = () => {
-        router.push("/signin")
-    }
+    const handleClick = async () => {
+        setIsLoading(true); // Set loading state to true before navigation
+        try {
+            await router.push("/signin"); // Perform navigation
+        } catch (error) {
+            console.error("Error during navigation:", error); // Handle errors
+        } finally {
+            // setIsLoading(false); // Set loading state to false after navigation (even on errors)
+        }
+    };
 
     if (mode == "modal") {
         return (
@@ -29,7 +38,8 @@ const LoginButton = ({
     }
 
     return (
-        <span className=' cursor-pointer' onClick={onClick}>
+        <span className=' cursor-pointer flex items-center gap-2' onClick={handleClick}>
+            <p><MoonLoader size={25} speedMultiplier={0.8} loading={isLoading} /></p>
             {children}
         </span>
     )
